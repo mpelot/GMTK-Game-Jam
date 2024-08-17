45854b729f7e40d99ef3ccc223ce8b2b81b113f1
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2024-08-17 12:59:35 -0400

    Trajectory Line

---
+++ Assets/Scripts/TrajectoryLine.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrajectoryLine : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public Core core;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2[] points = calculateTrajectoryPoints(100, 10);
+
+        Vector3[] convertedPoints = new Vector3[points.Length];
+        for (int i = 0; i < points.Length; i++)
+        {
+            convertedPoints[i] = points[i];
+        }
+        lineRenderer.positionCount = convertedPoints.Length;
+        lineRenderer.SetPositions(convertedPoints);
+    }
+
+    private Vector2[] calculateTrajectoryPoints(int pointCount, float timeMultiplier)
+    {
+        Vector2[] points = new Vector2[pointCount];
+        Vector2 position = transform.position;
+        Vector2 velocity = ((Vector2) core.transform.position - position).normalized * 0.8f;
+        float timeStep = Time.fixedDeltaTime * timeMultiplier;
+        for (int i = 0; i < pointCount; i++)
+        {
+            points[i] = position;
+
+            RaycastHit2D hit = Physics2D.Raycast(position, velocity, velocity.magnitude * timeStep);
+            if (hit.collider != null && hit.collider.CompareTag("Planet"))
+            {
+                Vector2 directionToPlanet = (hit.collider.transform.position - (Vector3)position).normalized;
+                float distanceToPlanet = Vector2.Distance(position, hit.collider.transform.position);
+
+
+                Vector2 gravitationalForce = directionToPlanet * 0.3f * (2.7f - distanceToPlanet);
+                velocity += gravitationalForce * timeStep;
+            }
+
+            position += velocity * timeStep;
+        }
+        return points;
+    }
+}
