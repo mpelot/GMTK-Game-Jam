a9de7d1f9d80db766b407244abfc5c9a37d582cc
mpelot <mwp81301@uga.edu>
2024-08-17 12:36:44 -0400

    Wave System

--- Assets/Scripts/GameMangager.cs
+++ Assets/Scripts/GameMangager.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class GameMangager : MonoBehaviour
 {
     private int _money;
+    public Spawner spawner;
     public int money
     {
         get
@@ -32,5 +33,20 @@ public class GameMangager : MonoBehaviour
         
     }
 
-    
+    IEnumerator SpawnWave() {
+
+        float angle = Random.Range(0, 360);
+        Vector3 position = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0f) * 10f;
+
+        //Alert
+
+        yield return new WaitForSeconds(3f);
+
+        Spawner sp = Instantiate(spawner, position, Quaternion.identity);
+        sp.Init(5);
+
+        yield return new WaitForSeconds(20f);
+
+        StartCoroutine(SpawnWave());
+    }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,25 +8,22 @@ public class Spawner : MonoBehaviour
     public Core core;
 
     // Start is called before the first frame update
-    void Start()
+    public void Init(int count)
     {
-        StartCoroutine(Spawn());
+        StartCoroutine(SpawnAsteroid(count));
     }
 
-    IEnumerator Spawn() {
-        //float angle = Random.Range(0, 360);
-        float angle = 50f;
-        Vector3 position = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0f) * 10f;
-        Asteroid ast = Instantiate(asteroid, position, Quaternion.identity);
-        ast.GetComponent<Rigidbody2D>().velocity = (core.transform.position - ast.transform.position).normalized * 0.8f;
+    IEnumerator SpawnAsteroid(int count) {
 
-        angle = 250f;
-        position = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0f) * 10f;
-        Asteroid ast2 = Instantiate(asteroid, position, Quaternion.identity);
-        ast2.GetComponent<Rigidbody2D>().velocity = (core.transform.position - ast2.transform.position).normalized * 0.8f;
+        if (count <= 0) {
+            Destroy(gameObject);
+        }
+
+        Asteroid ast = Instantiate(asteroid, transform.position, Quaternion.identity);
+        ast.GetComponent<Rigidbody2D>().velocity = (core.transform.position - ast.transform.position).normalized * 0.8f;
 
         yield return new WaitForSeconds(3f);
 
-        StartCoroutine(Spawn());
+        StartCoroutine(SpawnAsteroid(count - 1));
     }
 }
