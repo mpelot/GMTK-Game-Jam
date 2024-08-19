754d8ac1d4ee074fa2730f11b62f66864005a640
mpelot <mwp81301@uga.edu>
2024-08-19 02:16:57 -0400

    Main Menu

---
+++ Assets/Quit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] Color hoverColor;
+
+    private void OnMouseEnter() {
+        sr.color = hoverColor;
+    }
+
+    private void OnMouseExit() {
+        sr.color = Color.white;
+    }
+
+    private void OnMouseDown() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Start.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] Color hoverColor;
+
+    private void OnMouseEnter() {
+        sr.color = hoverColor;
+    }
+
+    private void OnMouseExit() {
+        sr.color = Color.white;
+    }
+
+    private void OnMouseDown() {
+        SceneManager.LoadScene(1);
+    }
+}
