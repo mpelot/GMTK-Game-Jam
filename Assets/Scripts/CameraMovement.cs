e2188da01e76e1ffaca3cd38249cba03c7812419
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2024-08-18 22:01:51 -0400

    Camera clamping

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,37 +8,63 @@ public class CameraMovement : MonoBehaviour
 
     [SerializeField] private float zoomStep, minSize, maxSize;
 
+    [SerializeField] private float minX, maxX, minY, maxY;
+
     private Vector3 dragOrigin;
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         PanCamera();
-        if (Input.mouseScrollDelta.y > .01f) {
+        if (Input.mouseScrollDelta.y > .01f)
+        {
             ZoomIn();
         }
-        else if (Input.mouseScrollDelta.y < -.01f) {
+        else if (Input.mouseScrollDelta.y < -.01f)
+        {
             ZoomOut();
         }
     }
 
-    private void PanCamera() {
+    private void PanCamera()
+    {
         if (Input.GetMouseButtonDown(2))
             dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButton(2)) {
+        if (Input.GetMouseButton(2))
+        {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
 
             cam.transform.position += difference;
+
+            ClampCameraPosition();
         }
     }
 
-    public void ZoomIn() {
+    public void ZoomIn()
+    {
         float newSize = cam.orthographicSize - zoomStep;
         cam.orthographicSize = Mathf.Clamp(newSize, minSize, maxSize);
+
+        ClampCameraPosition();
     }
 
-    public void ZoomOut() {
+    public void ZoomOut()
+    {
         float newSize = cam.orthographicSize + zoomStep;
         cam.orthographicSize = Mathf.Clamp(newSize, minSize, maxSize);
+
+        ClampCameraPosition();
+    }
+
+    private void ClampCameraPosition()
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = camHeight * cam.aspect;
+
+        float clampedX = Mathf.Clamp(cam.transform.position.x, minX + camWidth, maxX - camWidth);
+        float clampedY = Mathf.Clamp(cam.transform.position.y, minY + camHeight, maxY - camHeight);
+
+        cam.transform.position = new Vector3(clampedX, clampedY, cam.transform.position.z);
     }
 }
