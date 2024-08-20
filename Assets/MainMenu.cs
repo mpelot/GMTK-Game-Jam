a65ef40d4576015403ec08c91aeb0de62d333fc3
mpelot <mwp81301@uga.edu>
2024-08-20 04:59:19 -0400

    Update MainMenu.cs

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void MainMenuNow() {
         SFXPlayer.instance.PlaySFX("MenuHover");
+        Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(0);
         SceneManager.UnloadSceneAsync(1);
     }
